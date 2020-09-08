e2d4123d129b280b9207e2aba10601f53f54b1c0
kryhin <artem@maxtvmedia.com>
2020-09-07 20:39:35 -0400

    Initial Release

--- w2g-http-package.go
+++ w2g-http-package.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
+func SetupHeaderResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
